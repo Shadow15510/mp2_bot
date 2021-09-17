735f4ea3c4cc8790ef951791556a2f24b2fc4573
Shadow15510 <antoine.royer14@orange.fr>
2021-09-17 17:20:01 +0200

    Tâche

--- bot/mp2_bot.py
+++ bot/mp2_bot.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import json
 import os
 
@@ -16,11 +16,22 @@ mp2_bot = commands.Bot(command_prefix=config["PREFIX"])
 mp2_bot.add_cog(MPCommands(config))
 
 
+# Initialisation du statut personalisé
 @mp2_bot.event
 async def on_ready():
     activity = discord.Activity(type=discord.ActivityType.watching, name=config["PREFIX"] + "aide")
     await mp2_bot.change_presence(activity=activity)
     print("Connecté.")
+
+
+# Initialisation de la tâche
+@tasks.loop(seconds=300)
+async def cdp_implementation():
+    channel = client.get_channel(config["CDP_CHANNEL_ID"])
+    await channel.send("Message")
+
+# Lancement de la tâche
+cdp_implementation.start()
 
 
 # Lancement du bot
