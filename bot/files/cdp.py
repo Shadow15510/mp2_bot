f8693e456081cd976e241a0f63797bb634236454
Shadow15510 <antoine.royer14@orange.fr>
2021-09-18 15:46:37 +0200

    Implémentation de Cahier de prépa

---
+++ bot/files/cdp.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import json
+import os
+import requests
+import time
+
+
+def get_cdp_rss(lasttime):
+    payload = {"login": os.environ["cdp_login"], "motdepasse": os.environ["cdp_password"], "connexion": 1}
+
+    r = requests.post("https://cahier-de-prepa.fr/mp2-malherbe/ajax.php", data=payload)
+    cookies = {"CDP_SESSION": r.cookies["CDP_SESSION"]}
+
+    cdp = requests.get("https://cahier-de-prepa.fr/mp2-malherbe/rss/2d2f630bda45d7b1d0c3/rss.xml", cookies=cookies)
+
+    cdp = BeautifulSoup(cdp.text, features="html5lib")
+    cdp = cdp.find_all("item")
+
+    rss = []
+    for item in cdp:
+        item_time = item.select_one("pubDate").text
+        item_time = time.strptime(item_time[: item_time.find("+")], "%a, %d %b %Y %H:%M:%S ")
+
+        times = []
+        if item_time >= lastime:
+            times.append(item_time)
+            title = item.select_one("title").text
+            link = item.text.splitlines()[2]
+            rss.append((
+                title[9: title.find("]]>", 10)],
+                link.strip()
+            ))
+
+        return rss, max(times)
+
--- bot/mp2_bot.py
+++ bot/mp2_bot.py
@@ -4,6 +4,7 @@ import json
 import os
 
 from bot.files.commands import MPCommands
+from bot.files.cdp import get_cdp_rss
 
 
 # Chargement du fichier de configuration
@@ -24,11 +25,17 @@ async def on_ready():
     # cdp_implementation.start()
     print("Connecté.")
 
-# Initialisation de la tâche
-# @tasks.loop(seconds=5)
-# async def cdp_implementation():
-#     channel = mp2_bot.get_channel(id=config["CDP_CHANNEL_ID"])
-#     await channel.send("Message")
+Initialisation de la tâche
+@tasks.loop(seconds=10)
+async def cdp_implementation():
+    if config["SEND"]:
+        config["SEND"] = 0
+        with open("bot/config.json", "w") as file:
+            file.write(config)
+
+
+        channel = mp2_bot.get_channel(id=config["CDP_CHANNEL_ID"])
+        await channel.send("Message")
 
 # Lancement du bot
 mp2_bot.run(os.environ["token"])
