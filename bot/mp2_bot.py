b4081b773b9d224ade59ac235fb3b634de51f9d7
Shadow15510 <antoine.royer14@orange.fr>
2021-09-19 20:17:04 +0200

    Ajout de patterns pour le formatage des citations

--- bot/mp2_bot.py
+++ bot/mp2_bot.py
@@ -21,6 +21,9 @@ with open("bot/le_duff.tex", "r") as quotes_file:
         "\\lvert": "|",
         "\\rvert": "|",
         "\\cdot": "×",
+        "\\sin": "sin",
+        "\\cos": "cos",
+        "\\ln": "ln",
         
         "\t": "",
         "\\noindent": "",
