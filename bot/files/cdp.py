aa068d0a907c881f6a5332c5a8011b181fb35cc8
Shadow15510 <antoine.royer14@orange.fr>
2021-09-19 09:58:04 +0200

    Changement sur cahier de prépa

--- bot/files/cdp.py
+++ bot/files/cdp.py
@@ -7,8 +7,9 @@ import time
 
 def get_cdp_rss():
     # Récupération de la dernière date
-    lasttime = requests.get(f"https://odyssee.pythonanywhere.com/read/{os.environ['api_token']}").text
-    lasttime = time.strptime(lasttime, "%a, %d %b %Y %H:%M:%S")
+    lasttime = requests.get(f"https://odyssee.pythonanywhere.com/read/{os.environ["api_token"]}").text
+    print(lasttime)
+    lasttime = time.strptime(lasttime, "%d:%m:%H:%M")
 
     # Connexion au site et stockage du cookie de connexion
     payload = {"login": os.environ["cdp_login"], "motdepasse": os.environ["cdp_password"], "connexion": 1}
@@ -16,28 +17,20 @@ def get_cdp_rss():
     cookies = {"CDP_SESSION": r.cookies["CDP_SESSION"]}
 
     # Requête sur les flux RSS
-    cdp = requests.get("https://cahier-de-prepa.fr/mp2-malherbe/rss/2d2f630bda45d7b1d0c3/rss.xml", cookies=cookies)
+    cdp = requests.get("https://cahier-de-prepa.fr/mp2-malherbe/recent", cookies=cookies)
     cdp = BeautifulSoup(cdp.text, features="html5lib")
-    cdp = cdp.find_all("item")
+    cdp = cdp.find_all("article", {"class": "recents"})
 
-    # Analyse et stockage des informations
-    rss, times = [], []
-    for item in cdp:
-        item_time = item.select_one("pubDate").text
-        item_time = time.strptime(item_time[: item_time.find("+")], "%a, %d %b %Y %H:%M:%S ")
+    rss, doc_times = [], []
+    for document in cdp:
+        doc_time = time.strptime(document.select_one("p").text, "Publication le %d/%m à %Hh%M")
 
-        if item_time > lasttime:
-            times.append(item_time)
-            title = item.select_one("title").text
-            link = item.text.splitlines()[2]
-            rss.append((title[9: title.find("]]>", 10)], link.strip()))
+        if doc_time > lasttime:
+            doc_times.append(doc_time)
+            title = document.select_one("a")
+            rss.append((title.text, f"https://cahier-de-prepa.fr/mp2-malherbe/{title['href']}"))
 
     if rss:
-        # Sauvegarde de la dernière date
-        lasttime = max(times)
-        requests.get(f"https://odyssee.pythonanywhere.com/send/{os.environ['api_token']}/{time.strftime('%a, %d %b %Y %H:%M:%S', lasttime)}")
-
+        lasttime = max(doc_times)
+        requests.get(f"https://odyssee.pythonanywhere.com/send/{os.environ["api_token"]}/{time.strftime('%d:%m:%H:%M', lasttime)}")
         return rss
-
-
-
